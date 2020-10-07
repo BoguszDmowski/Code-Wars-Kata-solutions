5b4ea809c593075765cb396dffe9628dd54f8466
Bogusz Dmowski <58112552+BoguszDmowski@users.noreply.github.com>
2020-10-07 14:53:01 +0200

    Some updates

--- 7 kyu Kata.js
+++ 7 kyu Kata.js
@@ -1,3 +1,29 @@
+
+//############Mirror, mirror, on the wall...############
+function mirror(data) {
+  if (typeof data !== 'undefined' && data.length > 1) {
+    var firstList = [...data];
+    var secondList= [...data];
+    firstList.sort(function(a, b){return a-b});
+    secondList.sort(function(a, b){return b-a});
+    firstList.pop();
+    var thirdList = firstList.concat(secondList);
+    return thirdList;
+  } else {
+    return data
+  }
+}
+//############Stones on the Table############
+function solve(stones) {
+  var removedStones = 0;
+  var Arr = stones.split("");
+  for (i=0; i<Arr.length; i++) {
+    if (Arr[i]==Arr[i+1]) {
+      removedStones+=1;
+    } 
+  }
+  return removedStones
+};
 //############Hungarian Vowel Harmony (easy)############
 const front = ["e", "é", "i", "í", "ö", "ő", "ü", "ű"];
 const back = ["a", "á", "o", "ó", "u", "ú"];
